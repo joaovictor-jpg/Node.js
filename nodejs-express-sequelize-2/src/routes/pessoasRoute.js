d5382fe48528fd5d7ca1de8fcfebf6db85c5882b
João Victor <joaovictormdasilva676@gmail.com>
2024-09-19 22:39:27 -0300

    :wrench:feat: Adicionando mixins

--- nodejs-express-sequelize-2/src/routes/pessoasRoute.js
+++ nodejs-express-sequelize-2/src/routes/pessoasRoute.js
@@ -12,6 +12,7 @@ router.get('/pessoas/:id', (req, res) => pessoaController.buscarPorid(req, res))
 router.post('/pessoas', (req, res) => pessoaController.cadastrarPessoa(req, res));
 router.put('/pessoas/:id', (req, res) => pessoaController.atualizar(req, res));
 router.delete('/pessoas/:id', (req, res) => pessoaController.deletarPorId(req, res));
+router.get('/pessoas/:estudanteId/matriculas', (req, res) => pessoaController.pegaMatriculas(req, res));
 router.post('/pessoas/:estudanteid/matriculas', (req, res) => matriculaController.cadastrarPessoa(req, res));
 
 
