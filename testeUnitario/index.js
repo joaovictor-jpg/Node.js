5b8ccda54fe023fdb3142ad76a22b1d6ec75696d
João Victor <joaovictormdasilva676@gmail.com>
2024-09-26 21:46:00 -0300

    asserções

--- testeUnitario/index.js
+++ testeUnitario/index.js
@@ -1,5 +1,37 @@
-const somaValorHoraExtra = ( salario, valorHoraExtra ) => {
-  return salario + valorHoraExtra;
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const verifiqueSe = ( valor ) => {
+  const assercoes = {
+    ehExatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        throw {};
+      }
+    },
+  };
+  return assercoes;
 };
 
-console.log(somaValorHoraExtra(2000, 500));
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
+    console.log(`${titulo} passou!`);
+  } catch {
+    console.log(`${titulo} não passou!!!`);
+  }
+};
+
+teste("somaHorasExtras", () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
+
+teste("calculaDesconto", () => {
+  const esperado = 2300;
+  const retornado = calculaDescontos(2500, 200);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
