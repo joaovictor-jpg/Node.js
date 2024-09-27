314f15839c957c84de8104214958f19c903339d5
João Victor <joaovictormdasilva676@gmail.com>
2024-09-26 21:24:01 -0300

    Criando um novo projeto para testes

---
+++ testeUnitario/eslint.config.mjs
@@ -0,0 +1,77 @@
+import globals from "globals";
+
+
+
+import pluginJs from "@eslint/js";
+
+
+
+
+
+
+
+export default [
+
+
+
+  {
+
+
+
+    languageOptions: {
+
+
+
+      globals: {
+
+
+
+        ...globals.browser,
+
+
+
+        ...globals.node,
+
+
+
+      },
+
+
+
+    },
+
+
+
+    rules: {
+
+
+
+      indent: ["error", 2],
+
+
+
+      "linebreak-style": ["error", "windows"],
+
+
+
+      quotes: ["error", "double"],
+
+
+
+      semi: ["error", "always"],
+
+
+
+    },
+
+
+
+  },
+
+
+
+  pluginJs.configs.recommended,
+
+
+
+];
---
+++ testeUnitario/index.js
@@ -0,0 +1,5 @@
+const somaValorHoraExtra = ( salario, valorHoraExtra ) => {
+  return salario + valorHoraExtra;
+};
+
+console.log(somaValorHoraExtra(2000, 500));
