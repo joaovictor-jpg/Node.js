5d6c591e30a9a87e567360c563090702edf6a7ca
João Victor <81127942+joaovictor-jpg@users.noreply.github.com>
2024-10-14 10:51:08 -0300

    Update validadorGlobal.js

--- api-express-mongo/src/models/validadorGlobal.js
+++ api-express-mongo/src/models/validadorGlobal.js
@@ -3,5 +3,5 @@ import mongoose from "mongoose";
 
 mongoose.Schema.Types.String.set("validate", {
   validator: (valor) => valor.trim() !== "",
-  message: ({ path }) => `O campo ${path} foi fornecidop em branco.`
+  message: ({ path }) => `O campo ${path} foi fornecido em branco.`
 });
