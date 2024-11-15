f8ec17c4f3f0c842017fa9e83f5093b1291d0a7a
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2024-11-15 14:26:40 -0300

    TypeORM

--- loja/src/app.module.ts
+++ loja/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ProdutoModule } from './produto /produto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgressConfigServer } from './config/postgres.config.server';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule, ProdutoModule],
+  imports: [
+    UsuarioModule,
+    ProdutoModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgressConfigServer,
+      inject: [PostgressConfigServer],
+    }),
+  ],
 })
 export class AppModule {}
