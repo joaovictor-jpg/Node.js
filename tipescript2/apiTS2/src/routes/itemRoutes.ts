4895dabb8020b65a2c54198c2b9168219ebdfc41
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2025-07-25 23:20:36 -0300

    express.validation

--- tipescript2/apiTS2/src/routes/itemRoutes.ts
+++ tipescript2/apiTS2/src/routes/itemRoutes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { createItem, deleteItem, getItemById, getItems, updateItem } from '../controllers/itemController';
+import { body } from 'express-validator';
+import { isEmpty } from '../validation/nameIsEmpty';
 
 const router = Router();
 
 router.get('/', getItems);
 router.get('/:id', getItemById);
-router.post('/', createItem);
+router.post('/',
+  body('name').trim().notEmpty().withMessage('Name cannot be empty'),
+  isEmpty,
+  createItem
+);
 router.put('/:id', updateItem);
 router.delete('/:id', deleteItem);
 
